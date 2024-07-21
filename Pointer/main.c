e85dcad4da929268bf7edb803eab439fb4c9b440
urveeshdadhich <urveeshdadhich25@gmail.com>
2024-07-21 16:47:57 +0530

    Pointers

--- Pointer/main.c
+++ Pointer/main.c
@@ -3,7 +3,10 @@
 int main() {
     int a = 25;
     int *ptr = &a;
-    printf("The value of a = %d\n",*ptr);
-    printf("The address of a = %d\n",ptr);
+    printf("The value of a = %d\n",a);
+    printf("The value of a (using pointer) = %d\n",*ptr);
+    printf("The address of a = %x\n",&a);
+    printf("The address of a (using pointer) = %x\n",ptr);
+    printf("The address of pointer to a = %x\n",&ptr);
     return 0;
 }
